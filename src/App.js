2e3206dc53ef03cb5a84c9f578d6e1a1453eff73
abhiram <abhiram8034@gmail.com>
2022-06-22 21:19:59 +0000

    contacts added

---
+++ src/App.js
@@ -0,0 +1,117 @@
+import {Component} from 'react'
+
+import {v4 as uuidv4} from 'uuid'
+
+import ContactItem from './components/ContactItem'
+
+import './App.css'
+
+const initialContactsList = [
+  {
+    id: uuidv4(),
+    name: 'Ram',
+    mobileNo: 9542133025,
+    isFavorite: false,
+  },
+  {
+    id: uuidv4(),
+    name: 'Mani',
+    mobileNo: 90529285890,
+    isFavorite: true,
+  },
+  {
+    id: uuidv4(),
+    name: 'abhi',
+    mobileNo: 8247087256,
+    isFavorite: false,
+  },
+]
+
+class App extends Component {
+  state = {
+    contactsList: initialContactsList,
+    name: '',
+    mobileNo: '',
+  }
+
+  toggleIsFavorite = id => {
+    this.setState(prevState => ({
+      contactsList: prevState.contactsList.map(eachContact => {
+        if (id === eachContact.id) {
+          return {...eachContact, isFavorite: !eachContact.isFavorite}
+        }
+        return eachContact
+      }),
+    }))
+  }
+
+  onAddContact = event => {
+    event.preventDefault()
+
+    const {name, mobileNo} = this.state
+    const newContact = {
+      id: uuidv4(),
+      name,
+      mobileNo,
+      isfFavorite: false,
+    }
+
+    this.setState(prevState => ({
+      contactsList: [...prevState.contactsList, newContact],
+      name: '',
+      mobileNo: '',
+    }))
+  }
+
+  onChangeMobileNo = event => {
+    this.setState({mobileNo: event.target.value})
+  }
+
+  onChangeName = event => {
+    this.setState({name: event.target.value})
+  }
+
+  render() {
+    const {name, mobileNo, contactsList} = this.state
+    return (
+      <div className="app-container">
+        <div className="responsive-container">
+          <h1 className="heading">Contacts</h1>
+          <form className="contact-form-container" onSubmit={this.onAddContact}>
+            <input
+              value={name}
+              onChange={this.onChangeName}
+              className="input"
+              placeholder="Name"
+            />
+            <input
+              className="input"
+              value={mobileNo}
+              onChange={this.onChangeMobileNo}
+              placeholder="Mobile Number"
+            />
+            <button type="submit" className="button">
+              Add Contact
+            </button>
+          </form>
+          <ul className="contacts-table">
+            <li className="table-header">
+              <p className="table-header-cell name-column">Name</p>
+              <hr className="separator" />
+              <p className="table-header-cell">Mobile Number</p>
+            </li>
+            {contactsList.map(eachContact => (
+              <ContactItem
+                toggleIsFavorite={this.toggleIsFavorite}
+                key={eachContact.id}
+                contactDetails={eachContact}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
